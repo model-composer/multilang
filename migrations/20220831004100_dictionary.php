f7a194928fafa3be7ef12416fb7a87d8f380fa29
Domenico Giambra <zorinik@gmail.com>
2022-10-14 10:37:12 +0200

    New phinx format

--- migrations/20220831004100_dictionary.php
+++ migrations/20220831004100_dictionary.php
@@ -7,16 +7,16 @@ class Dictionary extends AbstractMigration
 	public function change()
 	{
 		$this->table('model_dictionary_sections')
-			->addColumn('name', 'string')
-			->addColumn('acl', 'enum', ['values' => ['user', 'root']])
+			->addColumn('name', 'string', ['null' => false])
+			->addColumn('acl', 'enum', ['null' => false, 'values' => ['user', 'root']])
 			->addIndex('name', ['unique' => true])
 			->create();
 
 		$this->table('model_dictionary')
-			->addColumn('section', 'string')
-			->addColumn('word', 'string')
-			->addColumn('lang', 'string')
-			->addColumn('value', 'string')
+			->addColumn('section', 'string', ['null' => false])
+			->addColumn('word', 'string', ['null' => false])
+			->addColumn('lang', 'string', ['null' => false])
+			->addColumn('value', 'string', ['null' => false])
 			->create();
 	}
 }
