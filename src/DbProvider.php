22ecb27eec9dc40066a76433fa81bb153dc70580
Domenico Giambra <zorinik@gmail.com>
2022-10-21 21:23:58 +0200

    Multilang fallback in selects

--- src/DbProvider.php
+++ src/DbProvider.php
@@ -5,6 +5,11 @@ use Model\Db\DbConnection;
 
 class DbProvider extends AbstractDbProvider
 {
+	private static array $tablesCache = [];
+
+	/**
+	 * @return array|\string[][]
+	 */
 	public static function getMigrationsPaths(): array
 	{
 		$config = Ml::getConfig();
@@ -16,9 +21,16 @@ class DbProvider extends AbstractDbProvider
 		] : [];
 	}
 
+	/**
+	 * @param DbConnection $db
+	 * @param string $table
+	 * @param array $where
+	 * @param array $options
+	 * @return array
+	 */
 	public static function alterSelect(DbConnection $db, string $table, array $where, array $options): array
 	{
-		$mlTables = Ml::getTables($db);
+		$mlTables = self::getTables($db);
 
 		if (isset($mlTables[$table])) {
 			$mlTableConfig = $mlTables[$table];
@@ -50,5 +62,78 @@ class DbProvider extends AbstractDbProvider
 		}
 
 		return [$where, $options];
+	}
+
+	/**
+	 * @param DbConnection $db
+	 * @param string $table
+	 * @param array $row
+	 * @param array $options
+	 * @return array
+	 */
+	public static function alterSelectResult(DbConnection $db, string $table, array $row, array $options): array
+	{
+		$config = Ml::getConfig();
+		$mlTables = self::getTables($db);
+
+		if (!$config['fallback'] or !isset($mlTables[$table]) or !($options['multilang_fallback'] ?? true))
+			return $row;
+
+		$mlTable = $mlTables[$table];
+
+		$tableModel = $db->getParser()->getTable($table);
+		if (!isset($row[$tableModel->primary[0]]))
+			return $row;
+
+		if (self::checkIfValidForFallback($row, $mlTable))
+			return $row;
+
+		foreach (($options['joins'] ?? []) as $idx => $join) {
+			if (isset($join['alias']) and in_array($join['alias'], ['lang', 'custom', 'custom_lang']))
+				unset($options['joins'][$idx]);
+		}
+
+		foreach ($config['fallback'] as $l) {
+			if ($options['lang'] === $l)
+				continue;
+
+			$mlRow = $db->select($table, $row[$tableModel->primary[0]], array_merge($options, [
+				'lang' => $l,
+				'multilang_fallback' => false,
+			]));
+			if ($mlRow and self::checkIfValidForFallback($mlRow, $mlTable))
+				return $mlRow;
+		}
+
+		return $row;
+	}
+
+	/**
+	 * @param DbConnection $db
+	 * @return array
+	 */
+	private static function getTables(DbConnection $db): array
+	{
+		if (!isset(self::$tablesCache[$db->getName()]))
+			self::$tablesCache[$db->getName()] = Ml::getTables($db);
+
+		return self::$tablesCache[$db->getName()];
+	}
+
+	/**
+	 * @param array $row
+	 * @param array $mlTable
+	 * @return bool
+	 */
+	private static function checkIfValidForFallback(array $row, array $mlTable): bool
+	{
+		$atLeastOne = false;
+		foreach ($mlTable['fields'] as $f) {
+			if (isset($row[$f]) and !empty($row[$f])) {
+				$atLeastOne = true;
+				break;
+			}
+		}
+		return $atLeastOne;
 	}
 }
